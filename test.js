4261d99685298c48ac9d54ad062fb3c16be21c12
Mike Atkins <apeherder@gmail.com>
2014-11-14 18:06:47 -0500

    Test with multiple plans; error handling in tests

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 var fftw = require('./index');
 var test = require('tape');
+var async = require('async');
 
 test('1d dft all ones', function(t) {
-  t.plan(8);
+  t.plan(10);
   fftw.plan_dft_1d({ size: 4 }, function(err, plan) {
+    t.error(err);
     plan.execute([1, 1, 1, 1], function(err, result) {
+      t.error(err);
       t.equal(result[0].real, 4);
       t.equal(result[0].imag, 0);
       t.equal(result[1].real, 0);
@@ -18,13 +21,16 @@ test('1d dft all ones', function(t) {
 });
 
 test('1d dft all ones with reused plan', function(t) {
-  t.plan(8);
+  t.plan(11);
   fftw.plan_dft_1d({ 
     size: 4,
     rigor: 'patient'
   }, function(err, plan) {
+    t.error(err);
     plan.execute([1, 1, 1, 1], function(err, result, plan) {
+      t.error(err);
       plan.execute([1, 1, 1, 1], function(err, result, plan) {
+        t.error(err);
         t.equal(result[0].real, 4);
         t.equal(result[0].imag, 0);
         t.equal(result[1].real, 0);
@@ -39,13 +45,15 @@ test('1d dft all ones with reused plan', function(t) {
 });
 
 test('1d idft one 1', function(t) {
-  t.plan(8);
+  t.plan(10);
 
   fftw.plan_dft_1d({ 
     size: 4, 
     sign: 'backward'
   }, function(err, plan) {
+    t.error(err);
     plan.execute([1, 0, 0, 0], function(err, result) {
+      t.error(err);
       result.forEach(function(complex) {
         t.equal(complex.real, 1);
         t.equal(complex.imag, 0);
@@ -54,15 +62,24 @@ test('1d idft one 1', function(t) {
   });
 });
 
-//TODO need async
-//test('1d forward reverse all ones', function(t) {
-//  t.plan(8);
-//  fftw.dft_1d([1, 1, 1, 1], function(err, result) {
-//    fftw.idft_1d(result, function(err, result) {
-//      result.forEach(function(complex) {
-//        t.equal(complex.real, 4);
-//        t.equal(complex.imag, 0);
-//      });
-//    });
-//  });
-//});
+test('1d forward reverse all ones', function(t) {
+  t.plan(11);
+  
+  async.map(
+    [{ size: 4, sign: 'forward' }, { size: 4, sign: 'backward' }],
+    fftw.plan_dft_1d, 
+    function(err, plans) {
+      t.error(err);
+      plans[0].execute([1, 1, 1, 1], function(err, result) {
+        t.error(err);
+        plans[1].execute(result, function(err, result) {
+          t.error(err);
+          result.forEach(function(complex) {
+            t.equal(complex.real, 4);
+            t.equal(complex.imag, 0);
+          });
+        });
+      });
+    }
+  );
+});
