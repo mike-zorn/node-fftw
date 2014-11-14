879f14ac83d2de42fe23f2a99f307639f69d6275
Mike Atkins <apeherder@gmail.com>
2014-11-13 19:28:06 -0500

    Verify that plan reuse works

--- test.js
+++ test.js
@@ -17,6 +17,24 @@ test('1d dft all ones', function(t) {
   });
 });
 
+test('1d dft all ones with reused plan', function(t) {
+  t.plan(8);
+  fftw.plan_dft_1d({ size: 4 }, function(err, plan) {
+    plan.execute([1, 1, 1, 1], function(err, result, plan) {
+      plan.execute([1, 1, 1, 1], function(err, result, plan) {
+        t.equal(result[0].real, 4);
+        t.equal(result[0].imag, 0);
+        t.equal(result[1].real, 0);
+        t.equal(result[1].imag, 0);
+        t.equal(result[2].real, 0);
+        t.equal(result[2].imag, 0);
+        t.equal(result[3].real, 0);
+        t.equal(result[3].imag, 0);
+      });
+    });
+  });
+});
+
 test('1d idft one 1', function(t) {
   t.plan(8);
 
